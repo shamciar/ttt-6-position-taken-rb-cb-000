076b70ab1c10ae00c632e242d4aff1320dc60383
Jake Matteson <jake.matteson@gmail.com>
2017-10-03 23:54:39 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,9 @@ def position_taken?(board, index)
   index = index.to_i
   if board[index - 1] == " " || board[index - 1] == ""
     !!false
-  else
+  elsif board[index - 1] == "X" || board[index - 1] == "O"
     !!true
+  else
+    nil
   end
 end
